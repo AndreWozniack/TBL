eabd7630d09da82f9f57fd6a21a357d306f9d2bf
André Wozniack <andre.wozniack@pucpr.edu.br>
2022-06-14 10:12:30 -0300

    Bora começa

--- Back-end.py
+++ Back-end.py
@@ -4,9 +4,4 @@ class Professor:
 
 class Disciplina:
     def __init__(self, **kwargs) -> None:
-<<<<<<< HEAD
         pass
-=======
-        pass
-
->>>>>>> 17063a75fd4017f441cf0d57c8e3e40d22126001
