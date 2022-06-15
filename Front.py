adfdf6ab03d3e368634cefc71d61f4154c3f2112
André Wozniack <andre.wozniack@pucpr.edu.br>
2022-06-15 17:24:44 -0300

    Atualização Carga Horaria

--- Front.py
+++ Front.py
@@ -3,11 +3,11 @@ import back
 
 sg.theme("DarkTeal4")
 def list_profs():
-    carga = sg.Text('Carga')
-    combo = sg.Combo(values=back.nomes(back.professores), key='Professor', size=(20,10))
+    carga = sg.Text('Carga', k='carga')
+    add_prof_bt = sg.Button(button_text='Add Prof')
     lt1 = [
-        [sg.Text('Professores:'),sg.Button(button_text='Add Prof')],
-        [combo, sg.Text('Carga Horaria'), carga],
+        [sg.Combo(values=back.nomes(back.professores), key='Professor', size=(20,10), enable_events=True), sg.Text('Carga Horaria'), carga],
+        [add_prof_bt],
         [sg.Text(f'_'*300)],
         [sg.Text('Selecione sua pasta:'), sg.InputText(key = 'TargetFolder'), sg.FolderBrowse('Pesquisar')],
         [sg.Submit(), sg.Exit()]
@@ -15,9 +15,15 @@ def list_profs():
     while True:
         w1 = sg.Window(title='Consultas', layout=lt1, size=(580,320))
         evento, dados = w1.read()
-        if evento == 'Add Prof':
-            back.add_prof(back.professores)
+        if evento == 'Professor':
+            for i in back.professores:
+                if i.nome == dados['Professor']:
+                    carga.update(f'{i.contaCarga()}')
+                    w1.refresh()
             print(dados['Professor'])
+            w1.refresh()
+        elif evento == 'Add Prof':
+            back.add_prof(back.professores)
         elif evento == sg.WIN_CLOSED or evento == 'Exit':
             break
         elif evento == 'Submit':
