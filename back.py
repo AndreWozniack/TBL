c8455c9ab1eac81a3094fc6372e8d55c52a15b08
frodeus <gustavo.munhoz015@gmail.com>
2022-06-19 20:46:46 -0300

    a

--- back.py
+++ back.py
@@ -80,10 +80,7 @@ def add_disc(x:list):
             nome = dados['Nome']
             carga_horaria = dados['Carga horaria']
             if eventos == 'Adicionar':
-                if nome_disc in nomes(disciplinas):
-                    sg.popup('Essa disciplina já existe!')
-                    break
-                else:
+                if nome_disc not in nomes(disciplinas):
                     x.append(Disciplina(nome = nome, carga = carga_horaria ))
                     sg.popup('Disciplina adicionada com sucesso!')
                     janela.close()
