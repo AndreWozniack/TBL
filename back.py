070f0fcbc7f2f3d04ba1b4c7403230d51fb738f6
frodeus <gustavo.munhoz015@gmail.com>
2022-06-19 21:57:06 -0300

    a

--- back.py
+++ back.py
@@ -139,8 +139,12 @@ def add_prof(x:list):
             pass
 
 def edit_disc(x:list):
-
-
+    layout = [
+        [sg.Text('Disciplina')]
+        [sg.ratio(carga_h)]
+    ]
+    janela = sg.Window('Editar disciplina', layout)
+    
 def criartxt(pasta):
     texto = []
     for i in professores:
